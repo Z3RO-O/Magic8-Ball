3331eb65aee873993896fbcb16210d69bb4bdc65
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2024-09-16 00:16:56 +0530

    getting response from model phi3

--- backend/src/app/crud.py
+++ backend/src/app/crud.py
@@ -1,26 +1,15 @@
-from langchain.prompts import PromptTemplate
-from langchain_community.llms import CTransformers
+import ollama
 
 # Function to get a Magic 8 Ball style response from the Phi-3 model
 def getMagic8BallAIResponse(question):
-    # Initialize the AI model (Phi-3)
-    llm = CTransformers(model='models/Phi-3-mini-4k-instruct-q4.gguf',
-                        model_type='phi',
-                        config={'max_new_tokens': 75, 'temperature': 0.001})  # Reduced temperature
-
-    # Define the prompt template to generate a Magic 8 Ball style response
-    template = """
-    You are a Magic 8 Ball that only responds with very short, cryptic answers like "Yes", "No", "Maybe", or "Ask again later". 
+    prompt = f"""
+    You are a Magic 8 Ball. Respond with a single cryptic answer such as "Yes", "No", "Maybe", "Ask again later", or similar brief phrases.
     Do not provide any additional text or explanation. 
-    Only provide one of these short answers to the question: "{question}"
+    For example: question is "Will I have a good day?" and the response can be "Yes maybe you will".
+    Just give a short response to the question: "{question}"
     """
-
-    prompt = PromptTemplate(input_variables=["question"], template=template)
+    response = ollama.generate(model='phi3', prompt=prompt)
     
-    # Format the prompt with the user's question
-    formatted_prompt = prompt.format(question=question)
-
-    # Generate the response using the Phi-3 model
-    response = llm.invoke(formatted_prompt)
+    # Assuming the response is a dictionary, access the 'response' key
     print(response)
-    return response.strip()
+    return response.get('response', 'No response available')
